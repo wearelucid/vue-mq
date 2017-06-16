bd4e8e6db8df0556974bcf508af369f60de50ece
David Simon <me@davidsimon.ch>
2017-06-16 19:01:08 +0200

    [WIP] Vue-MQ

---
+++ README.md
@@ -0,0 +1,66 @@
+# Vue-MQ
+Note: Draft, not quite production ready.
+
+Vue.js 2.0+ plugin for media queries.
+
+
+## Installation
+
+(TODO)
+
+## Integration
+
+```
+
+import { MediaQueries } from 'plugins/mediaqueries'
+
+const breakpoints = {
+  small: 400,
+  medium: 768,
+  large: 1100,
+  huge: 1400
+}
+
+Vue.use(MediaQueries, {
+  breakpoints
+})
+
+```
+
+And then use it in your components
+```
+// JSX Example
+Vue.component({
+  // ...
+  render (h) {
+    {this.$query({ from: 'medium' })
+      ? 'Small Content'
+      : 'Large Content'}
+  }
+})
+```
+Or display the currentBreakpoint
+```
+// JSX Example
+Vue.component({
+  name: 'DevStats'
+  // ...
+  render (h) {
+    Current Breakpoint: {this.$mq.currentBreakpoint.name} @ {this.$mq.currentBreakpoint.value}px
+  }
+})
+```
+
+## Todos
+- [ ] Consider removing `throw` in production
+- [ ] Validate reactive property. Is this correctly implemented? (_mq, __mq__, $mq)
+- [ ] Documentation: Example
+- [ ] Option to sync with CSS (e.g by reading breakpoints from JSON String, which we set <title>'s font-size property)
+- [ ] Implement alternative way to pass in breakpoints
+- [ ] Tests
+- [ ] Npm Compat
+
+## Browser Support
+IE9+ ([See clientWidth](https://tylercipriani.com/blog/2014/07/12/crossbrowser-javascript-scrollbar-detection/))
+
+## License
---
+++ src/main.js
@@ -0,0 +1,124 @@
+import debounce from 'lodash.debounce'
+
+/**
+ * MediaQueries v1.0.0
+ * License: WTFPL
+ *
+ * This is a media queries plugin for vue that adds a `$query` method to every
+ * component and exposes media query information through the `$mq` property.
+ *
+ * It assumes an option `breakpoints` with breakpoint names as keys
+ * and the width in pixels as number.
+ *
+ * TODO: Consider removing `throw` in production
+ * TODO: Validate reactive property. Is this correctly implemented? (_mq, __mq__, $mq)
+ * TODO: Documentation: Example
+ * TODO: Option to sync with CSS (e.g by reading breakpoints from JSON String, which we set <title>'s font-size property)
+ * TODO: Implement alternative way to pass in breakpoints
+ */
+
+export const MediaQueries = {
+  install (Vue, opt = {}) {
+    const options = {
+      debounceDelay: 100,
+      ...opt
+    }
+
+    if (typeof options.breakpoints !== 'object') { // TODO lodash.isobject
+      throw new Error('No valid breakpoints object received')
+    }
+
+    Object.defineProperty(Vue.prototype, '$mq', {
+      get () { return this.$root.__mq__ }
+    })
+
+    const localStore = {
+      listening: false,
+      width: false,
+      breakpoints: options.breakpoints,
+      currentBreakpoint: {
+        value: 0,
+        name: 'zero'
+      }
+    }
+
+    const resizeListener = debounce(function () {
+      updateBreakpoint()
+    }, options.debounceDelay)
+
+    const getBreakpointValue = function (breakpoint) {
+      if (localStore.breakpoints[breakpoint] === undefined) {
+        throw new Error('Breakpoint not found: "' + breakpoint + '"')
+      }
+
+      return parseInt(localStore.breakpoints[breakpoint], 10)
+    }
+
+    const updateBreakpoint = function () {
+      const { width } = getViewportSize()
+      const currentBreakpoint = { ...localStore.currentBreakpoint }
+      // Go through breakpoints and compare with window width
+      Object.keys(localStore.breakpoints).forEach((key) => {
+        if (width > localStore.breakpoints[key]) {
+          currentBreakpoint.name = key
+          currentBreakpoint.value = localStore.breakpoints[key]
+        }
+      })
+      localStore.currentBreakpoint = currentBreakpoint
+    }
+
+    Vue.mixin({
+      beforeCreate () {
+        this._mq = localStore
+        Vue.util.defineReactive(this, '__mq__', this._mq)
+      },
+      mounted () {
+        if (!this.$mq.listening) {
+          this.$mq.listening = true
+          window.addEventListener('resize', resizeListener)
+          resizeListener()
+        }
+      },
+      methods: {
+        $query: function (options) {
+          if (options.from === undefined && options.to === undefined) {
+            throw new Error('No values for "to" or "from" received')
+          }
+
+          if (options.to !== undefined && options.from !== undefined) {
+            const breakpointFrom = getBreakpointValue(options.from)
+            const breakpointTo = getBreakpointValue(options.to)
+
+            // "from" cannot be larger than "to"
+            if (breakpointFrom > breakpointTo) {
+              throw new Error('Breakpoint ' + breakpointFrom + ' is larger than ' + breakpointTo + '')
+            }
+
+            // The breakpoint needs to be smaller than the "to" (exclusive)
+            // but larger or the same as "from" (inclusive)
+            return breakpointFrom <= this.$mq.currentBreakpoint.value && this.$mq.currentBreakpoint.value < breakpointTo
+          }
+
+          if (options.to !== undefined) {
+            // Breakpoint needs to smaller than the "to" (exclusive)
+            return this.$mq.currentBreakpoint.value < getBreakpointValue(options.to)
+          }
+
+          if (options.from !== undefined) {
+            // Breakpoint needs larger or the same as "from" (inclusive)
+            return this.$mq.currentBreakpoint.value >= getBreakpointValue(options.from)
+          }
+        }
+      }
+    })
+
+    /*
+     * Use document.documentElement.clientWidth or document.body.clientWidth
+     * Works in IE8 and leaves away scrollbar
+     */
+    function getViewportSize () {
+      const e = document.documentElement || document.body
+      return { width: e[ 'clientWidth' ], height: e[ 'clientHeight' ] }
+    }
+  }
+}
